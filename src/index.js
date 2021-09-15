02b48bb01ea9f26d234046123d53641183820052
dongwonnn <kkum9408@gmail.com>
2021-09-15 22:23:28 +0900

    Test: PR Test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from 'App'
 import './index.css'
 import store from 'store'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Provider store={store}>
